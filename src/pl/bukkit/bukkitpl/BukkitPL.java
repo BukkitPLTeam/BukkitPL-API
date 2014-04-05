75ac2e7223e4dfe6d7cd446f6ca0eebbb83c81d4
BukkitSmerf <BukkitSmerf@Gmail.com>
2014-04-05 19:38:51 +0200

    init

---
+++ src/pl/bukkit/bukkitpl/BukkitPL.java
@@ -0,0 +1,14 @@
+package pl.bukkit.bukkitpl;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Main class
+ * 
+ * @author BukkitSmerf
+ * 
+ */
+public class BukkitPL extends JavaPlugin
+{
+
+}
