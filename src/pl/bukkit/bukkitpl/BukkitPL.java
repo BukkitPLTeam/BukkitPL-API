120d6d04c0061f9693048da3495ed9c0c6876120
ElDzi <wiktor_dylewski@onet.eu>
2014-04-05 20:21:50 +0200

    Rozwinięcie :)

--- src/pl/bukkit/bukkitpl/BukkitPL.java
+++ src/pl/bukkit/bukkitpl/BukkitPL.java
@@ -6,9 +6,21 @@ import org.bukkit.plugin.java.JavaPlugin;
  * Main class
  * 
  * @author BukkitSmerf
+ * @author ElDzi
  * 
  */
 public class BukkitPL extends JavaPlugin
 {
+
+
+
+
+  public void onEnable(){
+     System.out.println("Uruchamianie BukkitPL_API)");
+  }
+
+  public void onDisable(){
+    System.out.println("Wylaczanie BukkitPL_API)");
+  }
 
 }
